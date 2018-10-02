9eefb91ec818fa430be831a1a6422058251f3c6d
vesdim2007 <vesdim2007@gmail.com>
2018-10-02 13:19:51 +0300

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
-  } else {
-    module.exports = require('./dev');
+module.exports = {
+  host : 'horton.elephantsql.com',
+  user : 'eesrrnlc',
+  password : 'nUFpxpfQMACKXnpfTJNfpPowbKW3Qil2',
+  database : 'eesrrnlc',
+  apiKey: '7a1dc0abe0134958be77256f30acf285',
+  secretOrKey: '32uth54tg9854435hgfewoit546i7j98uoir',
+  accessKeyId: 'AKIAIBOPLGXA2VNZG7CA',
+  secretAccessKey: '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'
 }
--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host : process.env.HOST,
-    user : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE,
-    apiKey: process.env.APIKey
-}
